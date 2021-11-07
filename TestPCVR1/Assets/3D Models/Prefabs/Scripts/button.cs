b79f6ce8b5d6d76773d48e73de4102edf2cc9e0a
Ruslan <resic.resic@yandex.ru>
2021-11-07 15:24:57 +0700

    анимация кнопки

---
+++ TestPCVR1/Assets/3D Models/Prefabs/Scripts/button.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour
+{
+    private Transform transform;
+    private Vector3 startPosition;
+    private Animation animation;
+    void Start()
+    {
+        transform = gameObject.GetComponent<Transform>();
+        startPosition = transform.position;
+        animation = gameObject.GetComponent<Animation>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision");
+        if (collision.collider.tag == "Hand")
+        {
+            animation.Play();
+        }
+    }
+}
